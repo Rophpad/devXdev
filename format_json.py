9ba0f26d0c5424ab7adb6b63371ad46d14ab27c4
Rophpad <rophenp@gmail.com>
2024-02-16 17:34:00 +0100

    conformed to pycodestyle

--- format_json.py
+++ format_json.py
@@ -5,25 +5,25 @@ import sys
 
 # Get file names from command line arguments
 if len(sys.argv) != 3:
-  print("Usage: python3 format_json.py <input_file> <output_file>")
-  # python3 format_json.py file.json file.json
-  sys.exit(1)  
-input_file = sys.argv[1] 
+    print("Usage: python3 format_json.py <input_file> <output_file>")
+    # python3 format_json.py file.json file.json
+    sys.exit(1)
+
+input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 # Read the JSON data
 with open(input_file) as f:
-  data = json.load(f)
+    data = json.load(f)
 
-# Format each dictionary 
+# Format each dictionary
 formatted_data = []
 for d in data:
-  formatted_dict = {} 
-  for k,v in d.items():
-    formatted_dict[k] = v
-  formatted_data.append(formatted_dict)
+    formatted_dict = {}
+    for k, v in d.items():
+        formatted_dict[k] = v
+    formatted_data.append(formatted_dict)
 
-# Write formatted data to file  
+# Write formatted data to file
 with open(output_file, 'w') as f:
-   json.dump(formatted_data, f, indent=2)
-
+    json.dump(formatted_data, f, indent=2)
